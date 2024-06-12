506c2a3cf9995fe68d831bbd8589ecab2e6a3bd3
justbobinaround <roberthenryjunkins@gmail.com>
2024-06-11 22:32:17 -0400

    figured out pwd problem

--- src/pty/forker.rs
+++ src/pty/forker.rs
@@ -1,13 +1,11 @@
 use libc::termios as Termios;
-use portable_pty::{native_pty_system, CommandBuilder, MasterPty, PtyPair, PtySize};
-use std::collections::HashMap;
-use std::process::Command;
+use std::ffi::CString;
+use libc::{pid_t, TIOCGSID, ioctl, readlink};
+use portable_pty::{native_pty_system, CommandBuilder, PtyPair, PtySize};
 use std::sync::{Arc, Mutex};
-use std::io::{self, Error, Read, Stdin, Stdout, Write};
-use std::thread::{self, JoinHandle};
-use std::time::Duration;
+use std::io::{self, Error, Read, Stdout, Write};
 use super::raw_mode::raw_mode;
-use crate::ascii::escapes::{self, Escape, EscapeWriter, Sequence};
+use crate::ascii::escapes::{Escape, Sequence};
 
 pub struct PTerminal{
     writer: Stdout,
@@ -49,7 +47,7 @@ impl PTerminal {
         };
 
 
-        let mut child = match pair.slave.spawn_command(cmd) {
+        let child = match pair.slave.spawn_command(cmd) {
             Ok(child) => {child},
             Err(_) => {return Err(Error::new(io::ErrorKind::Other, "failed to spawn process"))}
         };
@@ -126,8 +124,9 @@ impl PTerminal {
                     match key_buffer[0] as char {
                         'r' => {
                             if let Ok(mut p_term) = p_term_3.lock() {
-                                unimplemented!("need to figure out how to get process pwd");
-                                //p_term.respawn(&("/home/WORK/Downloads/".to_string()));
+                                if let Ok(pwd) = p_term.get_process_pwd() {
+                                    p_term.respawn(&pwd);
+                                }                            
                             }
                         }
                         'q' => {
@@ -164,6 +163,50 @@ impl PTerminal {
         Ok(p_term)
     }
 
+    pub fn get_process_pwd(&self) -> io::Result<String> {
+        if let Ok(child) = self.child.lock() {
+            if let Some(process_id) = self.pty_pair.master.process_group_leader() {
+            //if let Some(process_id) = child.process_id() {
+                let mut target = vec![0u8; 4096];
+
+
+                let path = format!("/proc/{}/cwd", process_id);
+
+                match PTerminal::read_link_to_buf(&path, &mut target) {
+                    Ok(n) if n > 0 => return Ok(String::from_utf8_lossy(&target[..n]).to_string()),
+                    Ok(_) => {},
+                    Err(_) => {}
+                }
+
+                let mut sid: pid_t = 0;
+                if unsafe { ioctl(process_id as i32, TIOCGSID, &mut sid) } != -1 {
+                    let path = format!("/proc/{}/cwd", sid);
+                    match PTerminal::read_link_to_buf(&path, &mut target) {
+                        Ok(n) if n > 0 => return Ok(String::from_utf8_lossy(&target[..n]).to_string()),
+                        Ok(_) => {},
+                        Err(_) => {}
+                    }
+                }
+
+                return Err(io::Error::new(io::ErrorKind::NotFound, "process id returned None"));
+            } else {
+                return Err(io::Error::new(io::ErrorKind::NotFound, "process id returned None"));
+            }
+        } else {
+            return Err(io::Error::new(io::ErrorKind::NotFound, "process id returned None"));
+        }
+    }
+
+    pub fn read_link_to_buf(path: &str, buf: &mut [u8]) -> io::Result<usize> {
+        let c_path = CString::new(path)?;
+        let n = unsafe { readlink(c_path.as_ptr(), buf.as_mut_ptr() as *mut libc::c_char, buf.len()) };
+        if n == -1 {
+            Err(io::Error::last_os_error())
+        } else {
+            Ok(n as usize)
+        }
+    }
+
     pub fn respawn(&mut self, working_dir: &String) -> io::Result<()>{
         let mut cmd = CommandBuilder::new("sh");
         cmd.arg("-c");
@@ -174,12 +217,9 @@ impl PTerminal {
             *child = match self.pty_pair.slave.spawn_command(cmd) {
                 Ok(child) => {child},
                 Err(_) => {
-                    println!("failed");
                     return Err(Error::new(io::ErrorKind::Other, "failed to spawn process"))}
             };
         }
-
-
         Ok(())
     }
 
